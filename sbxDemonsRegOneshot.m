546e6ecac2e8fd16ab4a6d526a0ff70c8aa0e069
Stephen Zhang <x.stephen.zhang@gmail.com>
2019-10-06 16:15:23 -0400

    Initial commit

---
+++ sbxDemonsRegOneshot.m
@@ -0,0 +1,153 @@
+function sbxDemonsRegOneshot(mouse, date, varargin)
+% sbxDemonsRegOneshot is a onestep application of demonsreg, with the
+% intention of improving speed.
+
+    p = inputParser;
+
+    % Lab variables variables
+    addOptional(p, 'target', []);  % Target run to align to, default runs(1)
+    addOptional(p, 'server', []);  % Add in the server name as a string
+    addOptional(p, 'runs', []);  % Defaults to sbxRuns
+    addOptional(p, 'force', false);  % Overwrite files if they exist
+    addOptional(p, 'pmt', 0, @isnumeric);  % Which PMT to use for analysis, 0-green, 1-red
+    addOptional(p, 'chunksize', 1000); % Chunk size for parallel processing. Decrease if RAM is an issue
+    
+    % IO variables
+    addOptional(p, 'movtype', 'xyreg');  % input type, can be xyreg, sbxreg, or sbx.
+    addOptional(p, 'saveastiff', false); % Save as tiff
+    
+    % Image-processing variables
+    addOptional(p, 'edges', [0 0 0 0]); % Edges of registration. Pixels outside the edges will not be registered
+    addOptional(p, 'refsize', 500, @isnumeric);  % Set the number of frames from which we make the reference
+    addOptional(p, 'refoffset', 500, @isnumeric);  % The offset in frames for the reference image, accounts for weirdness in the first few frames
+    addOptional(p, 'ref_downsample_xy', [], @isnumeric);  % Pixels to downsample in xy, will be set to downsample_xy if empty
+      
+    % Rarely changed image-processing variables
+    addOptional(p, 'medfilt2size', [2 2]); % Neighbor area for 2D median filter. Leave empty if no median filter
+    addOptional(p, 'highpassnorm', true); % Use highpass filter and local normalization before registration.
+    addOptional(p, 'hp_norm_sigmas', [8, 30], @isnumeric);  % sigmas for highpass filter and local normalization
+    addOptional(p, 'binbeforehighpassnorm', false); % Bin image before applying spatial filters. Recommended for imaging
+                                                    % systems with magenification (e.g., GRIN doublets)                                                    % magnification
+  
+    % Efficiency variables
+    addOptional(p, 'savewarp', true); % Save warp parameteres
+    addOptional(p, 'reuseref', false); % Use pre-calculated ref if can find it (must be the right size).
+    
+    
+    % Unpack if needed
+    if iscell(varargin) && size(varargin,1) * size(varargin,2) == 1
+        varargin = varargin{:};
+    end
+
+    parse(p, varargin{:});
+    p = p.Results;
+
+%% Input cleanup
+if isempty(p.target), p.target = p.runs(1); end
+    
+%% IO paths
+% sbx paths
+sbxpaths = cell(length(p.runs),1);
+for i = 1 : length(p.runs)
+    sbxpaths{i} = sbxPath(mouse, date, p.runs(i),'xyreg', 'server', p.server, 'pmt', p.pmt);
+end
+
+% ref paths
+[rpath, fname] = fileparts(sbxpaths{p.runs == p.target});        
+regpath = [fileparts(rpath) '\reg_demonsreg'];
+refname = [regpath '\' fname '_demonsreg.tif'];
+
+% output paths
+outpaths = cell(length(p.runs),1);
+for i = 1 : length(p.runs)
+    outpaths{i} = sprintf('%s_demonsreg-%i.sbxreg', sbxpaths{i}(1:end-7), p.pmt);
+end
+
+%% Target
+% get ref
+
+if exist(refname, 'file') && p.reuseref
+    % Re-use a ref file
+    fprintf('Using existing reference image...\n')
+    ref = readtiff(refname);
+else
+    % Calculate ref fresh
+    fprintf('Calculating reference image...')
+    ref = sbxAlignTargetCore(sbxpaths{p.runs == p.target}, p.pmt, p.ref_downsample_xy,...
+        p.refoffset, p.refsize, p.edges);
+    fprintf(' Done. \n')
+    writetiff(ref, refname, class(ref));
+end
+
+%% Registration
+for i = 1:length(sbxpaths)
+    % Get the path and info file
+    movpath = sbxpaths{i};
+    info = sbxInfo(movpath);
+    nframes = info.max_idx + 1;
+
+    % Parpool
+    openParallel;
+    
+    % Parallel processing
+    nchunks = ceil(nframes/p.chunksize);
+    
+    % Get the meshgrid to pass to all the workers
+    [xx, yy] = meshgrid(1 : info.sz(2), 1 : info.sz(1));
+    
+    % registration paths
+    [rpath, fname] = fileparts(movpath);
+    regpath = [fileparts(rpath) '\reg_demonsreg'];
+    
+    % Initialize registered data (using the grid file xx to get size)
+    fprintf('Initializing array...')
+    data_reg = uint16(zeros(size(xx)));
+    data_reg = repmat(data_reg, [1, 1, nframes]);
+    fprintf(' Done. \n')
+    
+    % Initialize a cell for registered data
+    data_reg_cell = cell(nchunks, 1);
+    
+    fprintf('Parallel registration...')
+    % Parallel processing
+    parfor c = 1 : nchunks
+        % savepath for warp files
+        savepath = [regpath '\Transforms\' fname '_Dtransform_' sprintf('%02d', c)];
+        
+        % Process data
+        data_reg_cell{c} = ...
+            uint16(sbxDemonsRegOneshotCore(movpath, savepath, (c-1)*p.chunksize+1, ...
+            p.chunksize, ref, xx, yy, 'pmt', p.pmt, 'ref_downsample_xy', p.ref_downsample_xy,...
+            'hp_norm_sigmas', p.hp_norm_sigmas, 'savewarp', p.savewarp, ...
+            'medfilt2size', p.medfilt2size, 'binbeforehighpassnorm', p.binbeforehighpassnorm,...
+            'highpassnorm', p.highpassnorm, 'edges', p.edges));
+    end
+    fprintf(' Done. \n')
+    
+    % Reconstruct image stack
+    fprintf('Reconstruct image stack...')
+    for c = 1 : nchunks
+        % time indices
+        i_start = (c-1) * p.chunksize + 1;
+        i_end = min(c * p.chunksize, nframes);
+        
+        % Reconstruct
+        data_reg(:,:,i_start : i_end) = data_reg_cell{c};
+        
+    end
+    % Free memory
+    clear data_reg_cell
+    fprintf(' Done. \n')
+    
+    fprintf('Saving final stack...')
+    
+    % Save
+    if p.saveastiff
+        writetiff(data_reg,[outpaths{i}(1:end-7), '.tif'], 'double');  
+    else
+        sbxWrite(outpaths{i}, data_reg, info, p.force, true);
+    end
+end
+
+
+end
---
+++ sbxDemonsRegOneshotCore.m
@@ -0,0 +1,142 @@
+function data_reg = ...
+    sbxDemonsRegOneshotCore(mov_path, savepath, startframe, nframes, ref,...
+    meshxx, meshyy, varargin)
+%% sbxDemonsRegOneshotCore is the core of the namesake function, aiming to improve speed
+    
+    p = inputParser;
+    addOptional(p, 'pmt', 0, @isnumeric);  % REMEMBER, PMT is 0-indexed   
+    addOptional(p, 'ref_downsample_xy', 1, @isnumeric);
+    addOptional(p, 'hp_norm_sigmas', [8, 30], @isnumeric); % Sigma for gaussian fit
+    addOptional(p, 'savewarp', true);
+    addOptional(p, 'medfilt2size', [2 2]); % Neighbor area for 2D median filter
+    addOptional(p, 'highpassnorm', true); % Use highpass filter and local normalization before registration.
+    addOptional(p, 'binbeforehighpassnorm', false); % Bin image before applying spatial filters.
+                                                    % Recommended for
+                                                    % imaging systems with
+                                                    % magnification (e.g.,
+                                                    % GRIN doublets).
+    addOptional(p, 'edges', [0 0 0 0]); % Use edges to the processing (needed for bidirectional scanning).
+    parse(p, varargin{:});
+    
+    % Unpack if needed
+    if iscell(varargin) && size(varargin,1) * size(varargin,2) == 1
+        varargin = varargin{:};
+    end
+    
+    parse(p, varargin{:});
+    p = p.Results;
+
+%% Filter ref
+% Parameters
+n = p.hp_norm_sigmas(1);
+m = p.hp_norm_sigmas(2);
+
+% Median filter
+if ~isempty(p.medfilt2size)
+    ref = medfilt2(ref, p.medfilt2size, 'symmetric');
+end
+
+if p.highpassnorm
+    % Highpass and normalized
+    ref_prime = single(ref)-single(imgaussfilt(double(ref),n));
+    ref = ref_prime ./ (imgaussfilt(ref_prime.^2,m) .^ (1/2));
+
+    ref(isnan(ref)) = 0;
+end
+
+%% Read in data
+% Read in
+data = sbxReadPMT(mov_path, startframe - 1, nframes, p.pmt);
+
+c = class(data);
+
+%% Make a copy for pre-processing while keeping the original
+% For some reason uint16 gives crappy results
+% Apply edges if needed.
+data2 = single(data(p.edges(3)+1:end-p.edges(4), p.edges(1)+1:end-p.edges(2), :));
+
+%% Filter and bin data
+% Bin if necessary
+if p.binbeforehighpassnorm
+    if p.ref_downsample_xy > 1
+        data2 = binxy(data2, p.ref_downsample_xy);
+    end
+end
+
+if p.highpassnorm
+    for  i = 1:size(data2,3)
+        % Prepare the data by median filtering and local normalizing
+        if ~isempty(p.medfilt2size)
+            data2(:,:,i) = medfilt2(data2(:,:,i), p.medfilt2size, 'symmetric');
+        end
+        f_prime = data2(:,:,i) - imgaussfilt(single(data2(:,:,i)),n);
+        g_prime = f_prime ./ (imgaussfilt(f_prime.^2,m).^(1/2));
+
+        g_prime(isnan(g_prime)) = 0;
+
+        data2(:,:,i)=g_prime;
+
+    end
+end
+
+% Bin, if necessary
+if ~p.binbeforehighpassnorm
+    if p.ref_downsample_xy > 1
+        data2 = binxy(data2, p.ref_downsample_xy);
+    end
+end
+
+%% Regsitration
+% Initialize D_combined
+D_combined = zeros(size(data2,1), size(data2,2), size(data2,3) * 2);
+
+% Get the D tensor
+for  i = 1:size(data2,3)
+    % Demons reg
+    [D,~] = imregdemons(data2(:,:,i), ref, [32 16 8 4],...
+            'AccumulatedFieldSmoothing',2.5,'PyramidLevels',4,'DisplayWaitbar',false);
+    
+    D_combined(:, :, i*2-1 : i*2) = D; 
+end
+
+% resize back if necessary
+if p.ref_downsample_xy > 1
+    D_combined = imresize(p.ref_downsample_xy * D_combined, p.ref_downsample_xy); % resize to bring back to full size of movie
+end
+
+% Re-embed D-combined into the full-size version if using edges
+if any(p.edges ~= 0)
+    D_combined_full = zeros(size(data,1), size(data,2), size(data,3) * 2);
+    D_combined_full(p.edges(3)+1:end-p.edges(4), p.edges(1)+1:end-p.edges(2), :) = D_combined;
+    
+    % Use the new D_combined for subsequent processing
+    D_combined = D_combined_full;
+    clear D_combined_full
+end
+
+% Data2 is a large variable
+if p.ref_downsample_xy > 1
+    clear Data2;
+end
+
+%% Apply the warp to original movie
+% Initialize
+data_reg = zeros(size(data));
+
+% Apply
+for i = 1 : size(data, 3)
+    data_reg(:,:,i) = ...
+        interp2(meshxx, meshyy, single(data(:,:,i)),...
+        meshxx + D_combined(:,:,2*i-1), meshyy + D_combined(:,:,2*i));
+end
+
+% Cast if needed
+if ~strcmp(class(data_reg), class(data))
+    data_reg = cast(data_reg, c);
+end
+
+%% Save warp if needed
+if p.savewarp
+    writetiff(D_combined,[savepath '.tif'],'single'); 
+end
+end
