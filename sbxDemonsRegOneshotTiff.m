269a7e0da07713e952599bbeb88a7347ee53ce2e
Stephen Zhang <7657965+xzhang03@users.noreply.github.com>
2024-09-04 13:02:04 -0400

    Update sbxDemonsRegOneshotTiff.m

--- sbxDemonsRegOneshotTiff.m
+++ sbxDemonsRegOneshotTiff.m
@@ -18,6 +18,7 @@ function sbxDemonsRegOneshotTiff(mouse, date, varargin)
     
     % IO variables
     addOptional(p, 'movtype', 'OTtiff');  % input type, can be xyreg, sbxreg, or sbx.
+    addOptional(p, 'iterations', 1); % Multi-iteration registration
     
     % Image-processing variables
     addOptional(p, 'edges', [0 0 0 0]); % Edges of registration. Pixels outside the edges will not be registered
@@ -142,120 +143,130 @@ for i = 1:length(tiffpaths)
     % Get the path and info file
     movpath = tiffpaths{i};
     
-    % Read
-    im = readtiff(movpath);
+    for iter = 1 : p.iterations
         
-    % sizes
-    sz = size(im);
-    
-    % Make ref
-    if ~exist('ref', 'var') && (i == 1)
-        ref = single(median(im(p.edges(3)+1:end-p.edges(4),p.edges(1)+1:end-p.edges(2),...
-            p.refoffset:p.refoffset+p.refsize-1),3));
+        if iter == 1
+            % Read
+            im = readtiff(movpath);
+
+            % sizes
+            sz = size(im);
+            
+        else
+            im = data_reg;
+        end
+
+        % Make ref
+        if (~exist('ref', 'var') && (i == 1)) || (iter > 1)
+            ref = single(median(im(p.edges(3)+1:end-p.edges(4),p.edges(1)+1:end-p.edges(2),...
+                p.refoffset:p.refoffset+p.refsize-1),3));
+
+            % Write ref
+            writetiff(ref, refname); % Write unbinned because sometimes binning happens after local normalization
+        end
+
+        % Bin ref (pre)
+        if p.binbeforehighpassnorm && p.binxy > 1
+            ref = binxy(ref, p.binxy);
+        end
+
+        % Local normalize ref
+        ref = medfilt2(ref, p.medfilt2size, 'symmetric');
+        f_prime = ref - imgaussfilt(ref, p.hp_norm_sigmas(1));
+        ref = f_prime ./ (imgaussfilt(f_prime.^2, p.hp_norm_sigmas(2)).^(1/2));
+        ref(isnan(ref)) = 0;
+
+        % Bin ref (post)
+        if ~p.binbeforehighpassnorm && p.binxy > 1
+            ref = binxy(ref, p.binxy);
+        end
+
+        % Parallel processing
+        nchunks = ceil(sz(3)/p.chunksize);
+
+        % Get the meshgrid to pass to all the workers
+        [xx, yy] = meshgrid(1 : sz(2), 1 : sz(1));
+
+        % registration paths
+        [rpath, fname] = fileparts(movpath);
+        regpath = fullfile(fileparts(rpath), 'demonsreg');
+
+        % Make demonsreg folder if necessary
+        if ~exist(regpath, 'dir')
+            mkdir(regpath);
+        end 
         
-        % Write ref
-        writetiff(ref, refname); % Write unbinned because sometimes binning happens after local normalization
-    end
-    
-    % Bin ref (pre)
-    if p.binbeforehighpassnorm && p.binxy > 1
-        ref = binxy(ref, p.binxy);
-    end
-    
-    % Local normalize ref
-    ref = medfilt2(ref, p.medfilt2size, 'symmetric');
-    f_prime = ref - imgaussfilt(ref, p.hp_norm_sigmas(1));
-    ref = f_prime ./ (imgaussfilt(f_prime.^2, p.hp_norm_sigmas(2)).^(1/2));
-    ref(isnan(ref)) = 0;
-    
-    % Bin ref (post)
-    if ~p.binbeforehighpassnorm && p.binxy > 1
-        ref = binxy(ref, p.binxy);
-    end
-    
-    % Parallel processing
-    nchunks = ceil(sz(3)/p.chunksize);
-    
-    % Get the meshgrid to pass to all the workers
-    [xx, yy] = meshgrid(1 : sz(2), 1 : sz(1));
-    
-    % registration paths
-    [rpath, fname] = fileparts(movpath);
-    regpath = fullfile(fileparts(rpath), 'demonsreg');
-    
-    % Make demonsreg folder if necessary
-    if ~exist(regpath, 'dir')
-        mkdir(regpath);
-    end 
-    
-    % Initialize registered data (using the grid file xx to get size)
-    fprintf('Initializing array...')
-    tic;
-    data_reg = uint16(zeros(sz(1), sz(2)));
-    data_reg = repmat(data_reg, [1, 1, sz(3)]);
-    t = toc;
-    fprintf(' Done. Elapsed time: %i seconds.\n', round(t));
-    
-    % Initialize a cell for registered data
-    data_reg_cell = cell(nchunks, 1);
-    
-    fprintf('Parallel registration...')
-    tic;
-    
-    % Parallel or not
-    if p.parfor
-        M = p.nworkers;
-    else
-        M = 0;
-    end
-    
-    % Slice data
-    fprintf('Slicing data...')
-    tic;
-    for c = 1 : nchunks
-        data_reg_cell{c} = im(:,:,(c-1) * p.chunksize + 1 :...
-            min(c * p.chunksize, sz(3)));
-    end
-    t = toc;
-    fprintf(' Done. Elapsed time: %i seconds.\n', round(t));
-    
-    % Parallel processing
-%     for c = 1 : nchunks
-    parfor (c = 1 : nchunks, M)
-        % savepath for warp files
-        savepath = [regpath '\Transforms\' fname '_Dtransform_' sprintf('%02d', c)];
+        if iter == 1
+            % Initialize registered data (using the grid file xx to get size)
+            fprintf('Initializing array...')
+            tic;
+            data_reg = uint16(zeros(sz(1), sz(2)));
+            data_reg = repmat(data_reg, [1, 1, sz(3)]);
+            t = toc;
+            fprintf(' Done. Elapsed time: %i seconds.\n', round(t));
+
+            % Initialize a cell for registered data
+            data_reg_cell = cell(nchunks, 1);
+        end
         
-        % Process data
-        data_reg_cell{c} = ...
-            uint16(sbxDemonsRegOneshotTiffCore(data_reg_cell{c}, savepath,...
-            ref, xx, yy, 'ref_downsample_xy', p.binxy,...
-            'hp_norm_sigmas', p.hp_norm_sigmas, 'savewarp', p.savewarp, ...
-            'medfilt2size', p.medfilt2size, 'binbeforehighpassnorm', p.binbeforehighpassnorm,...
-            'highpassnorm', p.highpassnorm, 'edges', p.edges, 'itr', p.itr, 'PyramidLevels', p.PyramidLevels,...
-            'AccumulatedFieldSmoothing', p.AccumulatedFieldSmoothing));
+        % Parallel or not
+        if p.parfor
+            M = p.nworkers;
+        else
+            M = 0;
+        end
+
+        % Slice data
+        fprintf('Slicing data...')
+        tic;
+        for c = 1 : nchunks
+            data_reg_cell{c} = im(:,:,(c-1) * p.chunksize + 1 :...
+                min(c * p.chunksize, sz(3)));
+        end
+        t = toc;
+        fprintf(' Done. Elapsed time: %i seconds.\n', round(t));
         
-        if ~p.parfor
-            fprintf('Chunk %i/%i registered.\n', c, nchunks);
+        fprintf('Parallel registration...')
+        tic
+        
+        % Parallel processing
+        parfor (c = 1 : nchunks, M)
+            % savepath for warp files
+            savepath = [regpath '\Transforms\' fname '_Dtransform_' sprintf('%02d', c)];
+
+            % Process data
+            data_reg_cell{c} = ...
+                uint16(sbxDemonsRegOneshotTiffCore(data_reg_cell{c}, savepath,...
+                ref, xx, yy, 'ref_downsample_xy', p.binxy,...
+                'hp_norm_sigmas', p.hp_norm_sigmas, 'savewarp', p.savewarp, ...
+                'medfilt2size', p.medfilt2size, 'binbeforehighpassnorm', p.binbeforehighpassnorm,...
+                'highpassnorm', p.highpassnorm, 'edges', p.edges, 'itr', p.itr, 'PyramidLevels', p.PyramidLevels,...
+                'AccumulatedFieldSmoothing', p.AccumulatedFieldSmoothing));
+
+            if ~p.parfor
+                fprintf('Chunk %i/%i registered.\n', c, nchunks);
+            end
+        end
+        t = toc;
+        fprintf(' Done. Elapsed time: %i seconds.\n', round(t));
+
+        % Reconstruct image stack
+        fprintf('Reconstruct image stack...')
+        tic;
+        for c = 1 : nchunks
+            % time indices
+            i_start = (c-1) * p.chunksize + 1;
+            i_end = min(c * p.chunksize, sz(3));
+
+            % Reconstruct
+            data_reg(:,:,i_start : i_end) = data_reg_cell{c};
+
+            if ~p.parfor
+                fprintf('Chunk %i/%i reconstructed.\n', c, nchunks);
+            end
         end
     end
-    t = toc;
-    fprintf(' Done. Elapsed time: %i seconds.\n', round(t));
     
-    % Reconstruct image stack
-    fprintf('Reconstruct image stack...')
-    tic;
-    for c = 1 : nchunks
-        % time indices
-        i_start = (c-1) * p.chunksize + 1;
-        i_end = min(c * p.chunksize, sz(3));
-        
-        % Reconstruct
-        data_reg(:,:,i_start : i_end) = data_reg_cell{c};
-        
-        if ~p.parfor
-            fprintf('Chunk %i/%i reconstructed.\n', c, nchunks);
-        end
-    end
     % Free memory
     clear data_reg_cell
     t = toc;
